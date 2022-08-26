e239d146549cacfab96087499e65aa0e8c338071
Emily <emily@oxen.io>
2022-08-26 11:21:26 +1000

    start of disappearing messages test

---
+++ run/test/specs/disappearing_messages.spec.ts
@@ -0,0 +1,19 @@
+describe("Disappearing messages", () => {
+  it("Set disappearing messages", async () => {
+    // Open app
+    // Create user A and user B
+    // Create contact
+    // Click conversation options menu (three dots)
+    // Select disappearing messages option
+    // Select 5 seconds
+    // Select OK
+    // Check config message for User A
+    // Check config message for User B
+    // Send message
+    // Wait 5 seconds
+    // Look for message for User A
+    // Look for message for User B
+    // Turn off disappearing messages
+    // Click on timer icon
+  });
+});
