7e03c812b89ad375e027b6d29d899d0fa828ad38
Emily <emily@oxen.io>
2024-07-03 14:18:13 +1000

    Updates some discrepancies from latest android 1.18.5 release

--- run/test/specs/utils/create_group.ts
+++ run/test/specs/utils/create_group.ts
@@ -47,7 +47,7 @@ export const createGroup = async (
     })
   );
   await runOnlyOnAndroid(platform, () =>
-    device1.waitForControlMessageToBePresent("You created a new group.", 5000)
+    device1.waitForControlMessageToBePresent("You created a new group.", 10000)
   );
   // Send message from User A to group to verify all working
   await device1.sendMessage(userAMessage);
