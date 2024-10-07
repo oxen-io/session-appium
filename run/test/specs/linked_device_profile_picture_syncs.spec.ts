5c6d85c276b5565ced2a9a9df4cfd0da41f0d703
Emily <emily@oxen.io>
2024-10-07 17:22:02 +1100

    Fixes update profile picture test to account for different colour values

--- run/test/specs/linked_device_profile_picture_syncs.spec.ts
+++ run/test/specs/linked_device_profile_picture_syncs.spec.ts
@@ -9,44 +9,51 @@ androidIt('Avatar restored', avatarRestored);
 
 async function avatarRestored(platform: SupportedPlatformsType) {
   const { device1, device2 } = await openAppTwoDevices(platform);
-  const pixelHexColour = 'cbfeff';
+  let expectedPixelHexColour: string;
+  if (platform === 'android') {
+    expectedPixelHexColour = 'cbfeff';
+  } else if (platform === 'ios') {
+    expectedPixelHexColour = '04cbfe';
+  } else {
+    throw new Error('Platform not supported');
+  }
   await linkedDevice(device1, device2, 'Alice', platform);
   await device1.uploadProfilePicture();
   await sleepFor(5000);
   // Wait for change
   // Verify change
   // Take screenshot
-  const el = await device1.waitForTextElementToBePresent({
+  const profilePicture = await device1.waitForTextElementToBePresent({
     strategy: 'accessibility id',
     selector: 'User settings',
   });
   await device2.clickOnByAccessibilityID('User settings');
   await runOnlyOnIOS(platform, () => device1.waitForLoadingOnboarding());
   await runOnlyOnAndroid(platform, () => sleepFor(10000));
-  const base64 = await device1.getElementScreenshot(el.ELEMENT);
-  const pixelColor = await parseDataImage(base64);
-  // console.log('Base64 value is', base64);
-  console.log('RGB Value of pixel is:', pixelColor);
-  if (pixelColor === pixelHexColour) {
+  const base64 = await device1.getElementScreenshot(profilePicture.ELEMENT);
+  const actualPixelColor = await parseDataImage(base64);
+  if (actualPixelColor === expectedPixelHexColour) {
     console.log('Device1: Colour is correct');
   } else {
-    throw new Error("Device1: Colour isn't cbfeff, it is: " + pixelColor);
+    throw new Error(`Device1: Colour isn't ${expectedPixelHexColour}, it is: ` + actualPixelColor);
   }
   console.log('Now checking avatar on linked device');
   // Check avatar on device 2
-  const el2 = await device2.waitForTextElementToBePresent({
+  const profilePictureLinked = await device2.waitForTextElementToBePresent({
     strategy: 'accessibility id',
     selector: 'User settings',
   });
   await sleepFor(3000);
   await device2.clickOnElementAll(new ExitUserProfile(device2));
   await device2.clickOnByAccessibilityID('User settings');
-  const base64A = await device2.getElementScreenshot(el2.ELEMENT);
-  const pixelColorLinked = await parseDataImage(base64A);
-  if (pixelColorLinked === pixelHexColour) {
+  const base64A = await device2.getElementScreenshot(profilePictureLinked.ELEMENT);
+  const actualPixelColorLinked = await parseDataImage(base64A);
+  if (actualPixelColorLinked === expectedPixelHexColour) {
     console.log('Device 2: Colour is correct on linked device');
   } else {
-    throw new Error("Device1: Colour isn't cbfeff, it is: " + pixelColor);
+    throw new Error(
+      `Device1: Colour isn't ${expectedPixelHexColour}, it is: ` + actualPixelColorLinked
+    );
   }
   await closeApp(device1, device2);
 }
